e8b5b226889ed67988196fe618939d8b4d51c812
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-12-13 00:17:25 -0300

    BackEnd(Build): Se agrega la estructura de Spring Boot
    Se arma la estructura de Spring Boot sacando los archivos autogenerados desde la pagina oficial

---
+++ src/main/java/com/proyectoaplicacion/GestionProduccion/GestionProduccionApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoaplicacion.GestionProduccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoaplicacion/GestionProduccion/GestionProduccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoaplicacion.GestionProduccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
