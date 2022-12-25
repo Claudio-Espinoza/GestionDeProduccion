cae7c32c0d9717301fc85c0f9164af23fa6449e8
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-12-25 15:22:20 -0300

    Re-Cambio de estructura
    Debido a las nuevas exigencias de nuestro proyecto, se ha de modificar la estructura

---
+++ Proceso/CreacionEstructura/src/test/java/ufro/dci/gestionproduccion/GestionProduccionApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.dci.gestionproduccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
