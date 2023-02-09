744ba9d5e49af43680505ca3a69216c1b56fb5af
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-02-09 10:08:21 -0300

    build(front/back): Integración y desarrollo de gestion y vistas de administrador

--- GestionProduccion/src/main/java/ufro/dci/gestionapp/controller/ManagementController.java
+++ GestionProduccion/src/main/java/ufro/dci/gestionapp/controller/ManagementController.java
@@ -21,13 +21,42 @@ public class ManagementController {
         return "manager/management/general-management";
     }
 
-    @GetMapping("/")
+    @GetMapping("/manager/info")
     public String showManagerData(Model model){
         String nombre = managerService.getEmployeeName();
         String apellido = managerService.getEmployeeLastName();
         model.addAttribute("nombre", nombre);
         model.addAttribute("apellido", apellido);
         return "/manager/management/general-management";
+    }
+    @PreAuthorize("hasAuthority('MANAGER')")
+    @GetMapping("/manager/clients")
+    public String viewClients(){
+        return "manager/management/view-clients";
+    }
+
+    @PreAuthorize("hasAuthority('MANAGER')")
+    @GetMapping("/manager/pizzas")
+    public String viewPizzas(){
+        return "manager/management/view-pizzas";
+    }
+
+    @PreAuthorize("hasAuthority('MANAGER')")
+    @GetMapping("/manager/drinks")
+    public String viewDrinks(){
+        return "manager/management/view-drinks";
+    }
+
+    @PreAuthorize("hasAuthority('MANAGER')")
+    @GetMapping("/manager/bakery")
+    public String viewBakery(){
+        return "manager/management/view-bakery";
+    }
+
+    @PreAuthorize("hasAuthority('MANAGER')")
+    @GetMapping("/manager/orders")
+    public String viewOrders(){
+        return "manager/management/view-orders";
     }
 
 
