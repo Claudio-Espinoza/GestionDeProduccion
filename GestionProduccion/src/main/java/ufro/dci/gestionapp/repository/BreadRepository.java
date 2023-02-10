1216f1a5efab06a989b287fa04b3c3ceeb2cb013
Claudio Espinoza Navas <c.espinoza28@ufromail.cl>
2023-02-10 14:09:00 -0300

    fixed(BackEnd): Cambio en las listas

--- GestionProduccion/src/main/java/ufro/dci/gestionapp/repository/BreadRepository.java
+++ GestionProduccion/src/main/java/ufro/dci/gestionapp/repository/BreadRepository.java
@@ -3,7 +3,6 @@ package ufro.dci.gestionapp.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ufro.dci.gestionapp.model.production.Bread;
-import ufro.dci.gestionapp.model.production.Drink;
 import ufro.dci.gestionapp.model.shooper.Shooper;
 
 import java.util.List;
--- GestionProduccion/src/main/java/ufro/dci/gestionapp/repository/ShooperRepository.java
+++ GestionProduccion/src/main/java/ufro/dci/gestionapp/repository/ShooperRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import ufro.dci.gestionapp.model.shooper.Shooper;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ShooperRepository extends CrudRepository<Shooper, Integer> {
